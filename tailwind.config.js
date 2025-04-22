b5c7cb079f50392240bbda15123ea06ec941e938
Ghost <64189955+m23xp@users.noreply.github.com>
2025-04-22 04:30:01 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#7c3aed',
+        secondary: '#1f1f1f',
+      },
+    },
+  },
+  plugins: [],
+}
